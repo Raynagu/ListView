90a08924104ef4217f5070989407dc1ef2e7a0ff
Raynagu <rayhubli@yahoo.com>
2020-02-25 23:29:15 +0530

    static list view

---
+++ app/src/main/java/com/admin/listview/DynamicList.kt
@@ -0,0 +1,12 @@
+package com.admin.listview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DynamicList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dynamiclist)
+    }
+}
