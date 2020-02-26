7f96c370c643c4b5c5a6ecb15540297d24efe603
Raynagu <rayhubli@yahoo.com>
2020-02-26 12:22:09 +0530

    add new item

--- app/src/main/java/com/admin/listview/DynamicActivity.kt
+++ app/src/main/java/com/admin/listview/DynamicActivity.kt
@@ -12,11 +12,23 @@ class DynamicActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dynamic)
 
-        val listItem = arrayOf("Android", "IOS", "Windows", "Linux")
+        val listItem = mutableListOf<String>("Android", "IOS", "Windows", "Linux")
+
         dynamicList.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItem)
 
         dynamicList.setOnItemClickListener { adapterView, view, i, l ->
             Toast.makeText(this, "clicked ${listItem[i]}", Toast.LENGTH_SHORT).show()
+        }
+
+        btn_newItem.setOnClickListener {
+            val newItem = edit_newItem.text.toString()
+            if(newItem.isEmpty()){
+                edit_newItem.error = "Please enter an item name"
+            }else{
+                listItem.add(newItem)
+                edit_newItem.text.clear()
+                dynamicList.adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listItem)
+            }
         }
     }
 }
